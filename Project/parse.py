6648a8e41314d433a4086d827b780076ceae8618
yugi957 <atul.krishnadas@outlook.com>
2023-11-08 15:56:34 -0600

    can read in testcase files now, getting some errors with character recognition inside assignments

--- Project/parse.py
+++ Project/parse.py
@@ -2,9 +2,21 @@ from antlr4 import *
 from PythonLexer import PythonLexer
 from PythonParser import PythonParser
 from PythonListener import PythonListener
+import sys
+
+def read_file(filename):
+    file = open(filename,'r')
+    code = file.read()
+    print(code)
+    file.close()
+    return code
 
 # Read your code from a file or from a string
-python_code = "yo = 3\n"
+if(len(sys.argv) != 2): test_file = "../TestCases/project_deliverable_1_testcase.py"
+else: test_file = "../TestCases/" + sys.argv[1]
+
+print(test_file)
+python_code = read_file(test_file)
 input_stream = InputStream(python_code)
 
 # Create a lexer and parser
